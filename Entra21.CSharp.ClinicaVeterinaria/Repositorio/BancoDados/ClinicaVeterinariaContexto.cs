d8530337d05536b625b405ae638ce8a01de251c0
willljdev <willljdev@gmail.com>
2022-08-22 20:16:17 -0300

    Atualização exercício

--- Entra21.CSharp.ClinicaVeterinaria/Repositorio/BancoDados/ClinicaVeterinariaContexto.cs
+++ Entra21.CSharp.ClinicaVeterinaria/Repositorio/BancoDados/ClinicaVeterinariaContexto.cs
@@ -1,10 +1,6 @@
 ﻿using Entra21.CSharp.ClinicaVeterinaria.Repositorio.Entidades;
+using Entra21.CSharp.ClinicaVeterinaria.Repositorio.Mapeamentos;
 using Microsoft.EntityFrameworkCore;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Entra21.CSharp.ClinicaVeterinaria.Repositorio.BancoDados
 {
@@ -20,9 +16,20 @@ namespace Entra21.CSharp.ClinicaVeterinaria.Repositorio.BancoDados
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-            base.OnModelCreating(modelBuilder);
+            //Documentação: https://docs.microsoft.com/pt-br/ef/core/managing-schemas/migrations/?tabs=dotnet-core-cli
+            //Necessário instalar a ferramenta do dotnet ef core
+            //dotnet tool install --global dotnet-ef
 
-            modelBuilder.Entity<Raca>().ToTable("racas");
+            //1a etapa criar a entidade Raca.cs
+            //2a etapa criar o mapeamento da entidade para tabela RacaMapeamento.cs
+            //3a etapa registrar mapeamento
+            //4a etapa gerar a migration
+            //5a etapa a migration poderá ser aplicada de duas formas:
+            //executar comando para aplicar a migration sem a necessidade de executar a plicação
+            //dotnet ef database update
+            //executar a aplicação irá aplicar a migration
+
+            modelBuilder.ApplyConfiguration(new RacaMapeamento());
         }
     }
 }
---
+++ Entra21.CSharp.ClinicaVeterinaria/Repositorio/Mapeamentos/RacaMapeamento.cs
@@ -0,0 +1,35 @@
+﻿using Entra21.CSharp.ClinicaVeterinaria.Repositorio.Entidades;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Entra21.CSharp.ClinicaVeterinaria.Repositorio.Mapeamentos
+{
+    //DB First: Criar uma aplicação onde o banco de dados já existe
+    //Code First: Criar um banco de dados apartir de uma aplicação existente
+    //Seed: Alimentar o banco de dados com registros
+    //Migration: representação do mapeamento que será aplicado no banco de dados
+    //Mapeamento: representação da entidade em tabelas, colunas, indices...
+    internal class RacaMapeamento : IEntityTypeConfiguration<Raca>
+    {
+        public void Configure(EntityTypeBuilder<Raca> builder)
+        {
+            builder.ToTable("racas");
+
+            builder.HasKey(x => x.Id).HasName("Id");
+
+            builder.Property(x => x.Especie)
+                .HasColumnType("VARCHAR")
+                .HasMaxLength(100)
+                .IsRequired() // not null
+                .HasColumnName("especie");
+
+            builder.Property(x => x.Nome)
+                .HasColumnType("VARCHAR")
+                .HasMaxLength(30)
+                .HasColumnName("nome")
+                .IsRequired();
+        }
+
+        
+    }
+}
